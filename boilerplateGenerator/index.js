5bbe8b543d3968c2f51b1f4cc3c8c2346860ca17
Murali Murugan <murali732000@gmail.com>
2024-06-18 20:52:05 +0100

    add

--- boilerplateGenerator/index.js
+++ boilerplateGenerator/index.js
@@ -66,12 +66,12 @@ function generateFullBoilerplate(generatorFilePath) {
   console.log("Boilerplate code generated successfully!");
 }
 
-const GENERATOR_FILE_PATHS = process.env.GENERATOR_FILE_PATHS?.split(' ') ?? [];
+const generatorFilePaths = process.env.GENERATOR_FILE_PATHS?.split(' ') ?? [];
 
-
-GENERATOR_FILE_PATHS.forEach((generatorFilePath) => {
-    const fullPath = path.join(__dirname, '..', 'problems', generatorFilePath);
-    generatePartialBoilerplate(fullPath);
-    generateFullBoilerplate(fullPath);
-  });
-  
+generatorFilePaths.forEach((filePath) => {
+    // Ensure we only run the generator on directories, not individual files
+    
+        generatePartialBoilerplate(path.join('problems', filePath));
+        generateFullBoilerplate(path.join('problems', filePath));
+    }
+);
